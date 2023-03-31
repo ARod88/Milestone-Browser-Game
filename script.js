44b88b7e0462a5444d4cf78bfe5443f14916b938
ARod88 <alma.rodriguez72@yahoo.com>
2023-03-30 21:32:49 -0700

    defined variables in javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+let words = [ 'jaguar', 'cat', 'piranah', 'lion', 'dog', 'lizard'];
+let numberofGuessess = 5;
+let wrongGuesses = 0;
+let selectedWword = words[Math.floor(Math.random() * words.length)];
+let wordElement = document.getElementById('.letter-container');
+console.log(wordElement);
+
+
+
+
+
+
+
