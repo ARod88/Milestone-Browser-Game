800ca9161d8493a4ea1ac4f2aa45c3651194a570
ARod88 <alma.rodriguez72@yahoo.com>
2023-04-02 23:51:18 -0700

    added images

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-let words = ['jaguar', 'cat', 'piranha', 'lion', 'dog', 'lizard'];
-let selectedWords = [];
-let wordDisplay = document.getElementById("word-display");
-wordDisplay.textContent = selectedWords;
+// array of words that will be using 
+let words = [ 'jaguar', 'cat', 'piranah', 'lion', 'dog', 'lizard'];
 
+//this is going to select random array 
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+// code that will decrease number of chances when the wrong button is selected
+let numberofChances = 5;
+
+function letterButtonSel() {
+  let selectedWord  = letterButtonSel.value;
+  // Check if guessedLetter is in selectedWord and update currentWordState
+  // ...
+
+  if (!selectedWord.includes(words)) {
+    numberofChances--;
+    numberofChancesDisplay.innerText = `Chances: ${numberofChances}`;
+    hangmanImage.setAttribute('src', `hangman-${lives}.png`);
+  }
+}
+
+
+// when button is selected it will get the letters 
 let buttons = document.querySelectorAll('.alpha-button');
 buttons.forEach(button => {
     button.addEventListener('click', event => {
-        let buttonText = event.target.textContent.toLowerCase();
-        if (words.includes(buttonText)) {
-            selectedWords.push(buttonText);
-            console.log(`Selected words: ${selectedWords}`);
-        }
+        let buttonClass = event.target.className;
+        console.log ('Button with class ${buttonClass} was clicked');
     });
 });
+
+
+
+
+
+let currentWord = Array(selectedWord.length).fill('_');
+// let alphaButtons = document.getElementsByClassName('.alpha-button')
+
+// for (let i = 0; i < alphaButtons.length; i++){
+//     alphaButtons[i].addEventListener('click'),function() {
+
+//     }
+// }
+
+// if (guessedLetters.includes(letter)) {
+//     return letter;
+//   } else {
+//     return '_';
+//   }
+
